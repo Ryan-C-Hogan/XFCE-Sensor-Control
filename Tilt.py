3af2a9c5bc3e10a3bc26eea8a9f0598caeb05f5d
Ryan Hogan <ryancharleshogan@gmail.com>
2020-05-31 15:31:08 -0400

    Update Tilt.py
    Added pointer device detection and device updating.
    Fixed variable names to match Python naming conventions.
    Moved code blocks into functions and slight reorganization.

--- Tilt.py
+++ Tilt.py
@@ -8,32 +8,76 @@ bus = SystemBus()
 sensor_proxy = bus.get('net.hadess.SensorProxy')
 
 # Transformation matrices for input
-__RIGHT__ = '0 1 0 -1 0 1 0 0 1'
-__LEFT__ = '0 -1 1 1 0 0 0 0 1'
-__NORMAL__ = '1 0 0 0 1 0 0 0 1'
-__INVERTED__ = '-1 0 1 0 -1 1 0 0 1'
+_right = '0 1 0 -1 0 1 0 0 1'
+_left = '0 -1 1 1 0 0 0 0 1'
+_normal = '1 0 0 0 1 0 0 0 1'
+_inverted = '-1 0 1 0 -1 1 0 0 1'
 
-# TODO: Figure out how to slap xinput data into here instead of hardcoding. Currently doesn't work for USB mice, nor portable.
-__DEVICE_LIST__ = ['SYNA2B31:00 06CB:7F8B Touchpad', 'Wacom HID 5113 Finger touch', 'Wacom HID 5113 Pen stylus',
-                   'Wacom HID 5113 Pen eraser', 'SYNA2B31:00 06CB:7F8B Mouse']
 
-__COMMANDS_LEFT__ = ["xrandr -o left"]
-__COMMANDS_RIGHT__ = ["xrandr -o right"]
-__COMMANDS_NORMAL__ = ["xrandr -o normal"]
-__COMMANDS_INVERTED__ = ["xrandr -o inverted"]
+def get_pointer_devices():
+    devices = subprocess.check_output("xinput --list --name-only",
+                                      shell=True,
+                                      executable='/bin/sh')
 
-# Pre-fills the lists of commands to activate for each orientation
-for device in __DEVICE_LIST__:
-    __COMMANDS_NORMAL__.append("xinput --set-prop \"" + device +
-                               "\" --type=float \"Coordinate Transformation Matrix\" " + __NORMAL__)
-    __COMMANDS_LEFT__.append("xinput --set-prop \"" + device +
-                             "\" --type=float \"Coordinate Transformation Matrix\" " + __LEFT__)
-    __COMMANDS_RIGHT__.append("xinput --set-prop \"" + device +
-                              "\" --type=float \"Coordinate Transformation Matrix\" " + __RIGHT__)
-    __COMMANDS_INVERTED__.append("xinput --set-prop \"" + device +
-                                 "\" --type=float \"Coordinate Transformation Matrix\" " + __INVERTED__)
+    devices = devices.decode('utf-8').split("\n")
+    device_list_ = []
+
+    i = 1
+    while "Virtual core keyboard" != devices[i]:
+        device_list.append(devices[i])
+        i += 1
+
+    return device_list_
+
+
+def generate_commands(devices):
+    commands_left = ["xrandr -o left"]
+    commands_right = ["xrandr -o right"]
+    commands_normal = ["xrandr -o normal"]
+    commands_inverted = ["xrandr -o inverted"]
+
+    for device in devices:
+        # Commands
+        commands_normal.append("xinput --set-prop \"" + device +
+                               "\" --type=float \"Coordinate Transformation Matrix\" " + _normal)
+        commands_left.append("xinput --set-prop \"" + device +
+                             "\" --type=float \"Coordinate Transformation Matrix\" " + _left)
+        commands_right.append("xinput --set-prop \"" + device +
+                              "\" --type=float \"Coordinate Transformation Matrix\" " + _right)
+        commands_inverted.append("xinput --set-prop \"" + device +
+                                 "\" --type=float \"Coordinate Transformation Matrix\" " + _inverted)
+
+    return commands_normal, commands_left, commands_right, commands_inverted
+
+
+# Returns true if a new device is found
+def check_new_devices(current_device_list):
+    devices = get_pointer_devices()
+    if len(devices) != len(current_device_list):
+        return True, devices
+    else:
+        return False
+
+
+device_list = get_pointer_devices()
+
+_commands = generate_commands(device_list)
+_commands_normal = _commands[0]
+_commands_left = _commands[1]
+_commands_right = _commands[2]
+_commands_inverted = _commands[3]
 
 while True:
+    # Checks if the command lists need to be updated
+    device_test = check_new_devices(device_list)
+    if device_test[0]:
+        device_list = device_test[1]
+        _commands = generate_commands(device_list)
+        _commands_normal = _commands[0]
+        _commands_left = _commands[1]
+        _commands_right = _commands[2]
+        _commands_inverted = _commands[3]
+
     sensor_proxy.ClaimAccelerometer()
     time.sleep(1)  # you have to wait a moment for the accelerometer to give you data
     orientation = str(sensor_proxy.AccelerometerOrientation)
@@ -41,13 +85,13 @@ while True:
     commands = []
 
     if orientation == "normal":
-        commands = __COMMANDS_NORMAL__
+        commands = _commands_normal
     elif orientation == "left-up":
-        commands = __COMMANDS_LEFT__
+        commands = _commands_left
     elif orientation == "right-up":
-        commands = __COMMANDS_RIGHT__
+        commands = _commands_right
     elif orientation == "bottom-up":
-        commands = __COMMANDS_INVERTED__
+        commands = _commands_inverted
 
     for command in commands:
         subprocess.call(command,
